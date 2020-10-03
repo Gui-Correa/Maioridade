823e33ad637524ca40d3a91d67f39c5011a83124
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 21:59:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio_5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a;
+
+			Console.WriteLine("Digite sua idade: ");
+			a = Convert.ToInt32(Console.ReadLine());
+
+			if (a > 18)
+			{
+				Console.WriteLine("Você é maior de idade");
+			}
+			if (a < 18)
+			{
+				Console.WriteLine("Você é menor de idade");
+			}
+			Console.ReadKey();
+		}
+	}
+}
